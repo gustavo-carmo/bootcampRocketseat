4f4ebcbcac77f7424597a03dbaaa6b848b6a57d9
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2020-05-20 23:32:09 -0300

    Start Authentication

---
+++ GoBarber/backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '9e7ff5ddfdf178ac259b1506029411aa',
+    expiresIn: '1d',
+  },
+};
---
+++ GoBarber/backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,51 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+import authConfig from '../config/auth';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+    const defaultError = 'Incorrect email/password combination.';
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error(defaultError);
+    }
+
+    const passwordCompared = await compare(password, user.password);
+
+    if (!passwordCompared) {
+      throw new Error(defaultError);
+    }
+
+    delete user.password;
+
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
